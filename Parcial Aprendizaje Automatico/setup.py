dd070cc2df119cf57780cd684fe3b92a9325b12f
AntoScotto <scottoantonela@gmail.com>
2025-10-16 19:00:35 -0300

    asd

--- Parcial Aprendizaje Automatico/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='Parcial Aprendizaje Automatico',
-    packages=find_packages(),
-    description='La identificación temprana y precisa de los asteroides que representan un riesgo de impacto con la Tierra es un problema de gran relevancia en la astronomía y la seguridad planetaria. La designación PHA depende de criterios complejos (como el Diámetro y la Mínima Distancia de Intersección Orbital, MOID)',
-    author='Antonela Scotto',
-    version='0.0.1'
-)
--- README.md
+++
@@ -1,8 +0,0 @@
-# Aprendizaje_Automatico
-
-
-Este es un repositorio para la entrega de tareas de Aprendizaje Automatico.
-Politecnico Malvinas Argentina
-2 año 1 Cuatrimestre
-Tec. Ciencia de Datos e Inteligencia Artificial
-
